fde6895e3a8215766eb1cc0274b45997792f7134
flofriday <flohacksfriday@gmail.com>
2024-03-02 21:47:27 +0100

    Add invite layout
    The code doesn't do anything yet but at least it looks good

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,20 +1,20 @@
-export default function Home() {
+export default function FAQ() {
   return (
     <main className="min-h-screen py-24 px-12 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold pb-6">Questions</h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pb-6">Questions</h1>
 
       <div className="pb-4">
-        <h2 className="text-xl font-bold">Did you really build this just for a birthday?</h2>
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">Did you really build this just for a birthday?</h2>
         <p>Sure.</p>
       </div>
 
       <div className="pb-4">
-        <h2 className="text-xl font-bold">Couldn't you just have used a normal spreadsheet or not written it down at all, like the cool kids</h2>
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">Couldn't you just have used a normal spreadsheet or not written it down at all, like the cool kids</h2>
         <p>Yes.</p>
       </div>
 
       <div className="pb-4">
-        <h2 className="text-xl font-bold">So why did you built it?</h2>
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">So why did you built it?</h2>
         <p>
           Partly, because I can and partly because for my last time I wrote
           a scientific paper as an invitation so this time I needed to up my
@@ -23,14 +23,11 @@ export default function Home() {
       </div>
 
       <div className="pb-4">
-        <h2 className="text-xl font-bold">Oh damn who else does stuff like that?</h2>
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">Oh damn who else does stuff like that?</h2>
         <p>
           No, it's just me for now.
         </p>
       </div>
-
-
-
 
     </main>
   );
