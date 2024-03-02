8ceca94c31277776b3f7e25767ac849a4cc3461a
flofriday <flohacksfriday@gmail.com>
2024-03-02 20:39:09 +0100

    Initial app structure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # birthdaiii
 An over engineered invitation management website
+
+
+## Run it yourself
+```
+npm install
+npm run dev
+```
---
+++ app/faq/page.tsx
@@ -0,0 +1,37 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen py-24 px-12 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-bold pb-6">Questions</h1>
+
+      <div className="pb-4">
+        <h2 className="text-xl font-bold">Did you really build this just for a birthday?</h2>
+        <p>Sure.</p>
+      </div>
+
+      <div className="pb-4">
+        <h2 className="text-xl font-bold">Couldn't you just have used a normal spreadsheet or not written it down at all, like the cool kids</h2>
+        <p>Yes.</p>
+      </div>
+
+      <div className="pb-4">
+        <h2 className="text-xl font-bold">So why did you built it?</h2>
+        <p>
+          Partly, because I can and partly because for my last time I wrote
+          a scientific paper as an invitation so this time I needed to up my
+          game, cause the competition is fierce!
+        </p>
+      </div>
+
+      <div className="pb-4">
+        <h2 className="text-xl font-bold">Oh damn who else does stuff like that?</h2>
+        <p>
+          No, it's just me for now.
+        </p>
+      </div>
+
+
+
+
+    </main>
+  );
+}
